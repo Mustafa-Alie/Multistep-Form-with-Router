07a7803a404ff07f04b84ce2b4049d2950bcddc4
Mustafa Ali <128284781+Mustafa-Alie@users.noreply.github.com>
2025-07-01 13:41:59 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: "/Multistep-Form/",
+  base: "/Multistep-Form-with-Router/",
   plugins: [react(), tailwindcss()],
 });
